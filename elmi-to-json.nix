4b1cb6c6aa9b193fb76eddbac2e4b938b9ced84f
Marek Fajkus <marek.faj@gmail.com>
2019-05-12 20:37:25 +0200

    add darwin bin support

--- elmi-to-json.nix
+++ elmi-to-json.nix
@@ -2,25 +2,22 @@
 
 let
   version = "0.19.3";
-  sha256 = "";
-
-  elmi-to-json =
-    pkgs.fetchFromGitHub {
-      rev = version;
-      owner = "stoeffel";
-      repo = "elmi-to-json";
-      sha256 = "0s32929q1xfqnrh5lv1xjhw5wmjdcm4c19hkdg8835px4kir9899";
-    };
 in with pkgs;
-  # (pkgs.haskellPackages.stack2nix "elmi-to-json-${version}" elmi-to-json {})
   { blob = stdenv.mkDerivation {
         name = "elmi-to-json-easy-${version}";
         inherit version;
 
-        src = fetchurl {
-          url = "https://github.com/stoeffel/elmi-to-json/releases/download/${version}/elmi-to-json-${version}-linux.tar.gz";
-          sha256 = "021zwpqicymxn2sn4m521a0awrv1zlfsm63hs648ldl8242lks4m";
-        };
+        src =
+          if stdenv.isDarwin then
+            fetchurl {
+              url = "https://github.com/stoeffel/elmi-to-json/releases/download/${version}/elmi-to-json-${version}-osx.tar.gz";
+              sha256 = "0hz3nr0bi0dlrxrwpnrv11q66acy77flz90f0p4bj65rypkrc7h6";
+            }
+          else
+            fetchurl {
+              url = "https://github.com/stoeffel/elmi-to-json/releases/download/${version}/elmi-to-json-${version}-linux.tar.gz";
+              sha256 = "021zwpqicymxn2sn4m521a0awrv1zlfsm63hs648ldl8242lks4m";
+            };
 
         unpackPhase = ''
           mkdir -p $out/bin
