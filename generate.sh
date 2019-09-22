202e1185ea40548e3dd034d5480aa5e1378853ec
Marek Fajkus <marek.faj@gmail.com>
2019-09-22 14:06:48 +0200

    update to node12

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@
 set -eu -o pipefail
 
 rm -f node-env.nix
-node2nix --nodejs-10 -i packages.json -o packages.nix -c composition.nix
+node2nix --nodejs-12 -i packages.json -o packages.nix -c composition.nix
